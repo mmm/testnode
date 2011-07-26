98c7683db30d0152909fef9de6c28e3e52f7dbf8
mmm <mmm@ensemble.inside.agiledynamics.com>
2011-07-25 18:54:40 -0600

    non-priv ports

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var server = http.createServer(function (request, response) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(80);
+server.listen(8000);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:80/");
+console.log("Server running at http://0.0.0.0:8000/");
 
