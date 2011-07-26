206fc06d2fe5ceae9d91b860d8f7b79300ab92c9
mmm <mmm@ensemble.inside.agiledynamics.com>
2011-07-26 07:57:18 -0600

    some cleanup

--- server.js
+++ server.js
@@ -6,20 +6,15 @@ var config = require('./config/config'),
     http = require('http');
 
 db = new mongo.Db('mynodeapp', new mongo.Server(config.mongo_host, config.mongo_port, {}), {});
-
 db.addListener("error", function(error) {
-  console.log("Error connecting to mongo -- perhaps it isn't running?");
+  console.log("Error connecting to mongo");
 });
 
-// Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
-
-// Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(config.listen_port);
 
-// Put a friendly message on the terminal
 console.log("Server running at http://0.0.0.0:8000/");
 
