3b112fcb434f77e464d1db0a54d1a147faf9d053
mmm <mmm@ensemble.inside.agiledynamics.com>
2011-07-26 09:27:36 -0600

    ugly but working

--- server.js
+++ server.js
@@ -5,43 +5,47 @@ var config = require('./config/config'),
     mongo = require('mongodb'),
     http = require('http');
 
-db = new mongo.Db('mynodeapp', new mongo.Server(config.mongo_host, config.mongo_port, {}), {});
-db.addListener("error", function(error) {
-  console.log("Error connecting to mongo");
-});
 
 var show_log = function(request, response){
-  db.open(function(err, connection){
-    connection.collection('addresses', function(err, collection){
-      collection.find({}, {limit:10, sort:[['_id','desc']]}, function(err, cursor){
-        cursor.toArray(function(err, items){
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.write("Hit-Log:\n");
-          for(i=0; i<items.length;i++){
-            res.write(JSON.stringify(items[i]) + "\n");
-          }
-          res.end();
-        });
-      });
-    });
+  var db = new mongo.Db('mynodeapp', new mongo.Server(config.mongo_host, config.mongo_port, {}), {});
+  db.addListener("error", function(error) {
+    console.log("Error connecting to mongo");
+  });
+  db.open(function(err, db){
+	    db.collection('addresses', function(err, collection){
+	      collection.find({}, {limit:10, sort:[['_id','desc']]}, function(err, cursor){
+		cursor.toArray(function(err, items){
+		  response.writeHead(200, {'Content-Type': 'text/plain'});
+		  response.write("Hit-Log:\n");
+		  for(i=0; i<items.length;i++){
+		    response.write(JSON.stringify(items[i]) + "\n");
+		  }
+		  response.end();
+		});
+	      });
+	    });
   });
 }
 
 var track_hit = function(request, response){
-  db.open(function(err, connection){
-    connection.collection('addresses', function(err, collection){
-      var address = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
-      hit_record = { 'ip': address, 'ts': new Date() };
+  var db = new mongo.Db('mynodeapp', new mongo.Server(config.mongo_host, config.mongo_port, {}), {});
+  db.addListener("error", function(error) {
+    console.log("Error connecting to mongo");
+  });
+  db.open(function(err, db){
+	    db.collection('addresses', function(err, collection){
+	      var address = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+	      hit_record = { 'ip': address, 'ts': new Date() };
 
-      collection.insert( hit_record, {safe:true}, function(err){
-        if(err) { 
-          console.log(err.stack);
-        }
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write(JSON.stringify(object_to_insert));
-        response.end("Tracked hit from " + request.connection.remoteAddress + "\n");
-      });
-    });
+	      collection.insert( hit_record, {safe:true}, function(err){
+		if(err) { 
+		  console.log(err.stack);
+		}
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.write(JSON.stringify(hit_record));
+		response.end("Tracked hit from " + address + "\n");
+	      });
+	    });
   });
 }
 
@@ -49,7 +53,7 @@ var track_hit = function(request, response){
 
 var server = http.createServer(function (request, response) {
 
-  var url = require('url').parse(req.url);
+  var url = require('url').parse(request.url);
 
   if(url.pathname === '/list') {
     show_log(request, response);
